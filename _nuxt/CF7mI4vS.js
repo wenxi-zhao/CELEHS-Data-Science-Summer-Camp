24d81224f7ac9dad57f08001178d5e8e4ab80760
wenxi-zhao <wenxi-zhao@users.noreply.github.com>
2025-02-06 08:59:23 +0000

    deploy: 3b55e0aee51d23081267f2587b0a39d17fc7d988

---
+++ _nuxt/CF7mI4vS.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./BORN8-eO.js";import"./DMhU7S1X.js";import"./C-v3KzvZ.js";import"./BGMNvqfX.js";import"./Dnd51l0P.js";import"./I-tHJLlU.js";export{o as default};
---
+++ _nuxt/ClJCiIv9.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./DMhU7S1X.js";const r={mounted(){this.$router.replace("/summer-camp")}};function c(s,n,a,p,_,m){return o(),t("div")}const i=e(r,[["render",c]]);export{i as default};
